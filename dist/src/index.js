c74b74d0605dfd8542b1dc6d9e72a4fc429b4e21
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-08-27 18:49:39 +0100

    tests won't fix.

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const findTrains_1 = require("./findTrains");
 const trackTrain_1 = require("./trackTrain");
@@ -15,16 +6,16 @@ module.exports = {
     findTrains: findTrains_1.default,
     trackTrain: trackTrain_1.trackTrain,
 };
-const util = require("util");
-// trackTrain("G59487").then((emitter) => {
-//   emitter.on("journey", (data) => {
-//     console.log(util.inspect(data, false, null, true));
-//   });
-//   emitter.on("information", (data) => {
-//     console.log(util.inspect(data, false, null, true));
-//   });
-// });
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    const data = yield (0, trackTrain_1.getHTML)("testServiceID", "2023-01-01");
-    console.log(`DATA IS THIS: ${data}`);
-}))();
+// const util = require("util");
+// // trackTrain("G59487").then((emitter) => {
+// //   emitter.on("journey", (data) => {
+// //     console.log(util.inspect(data, false, null, true));
+// //   });
+// //   emitter.on("information", (data) => {
+// //     console.log(util.inspect(data, false, null, true));
+// //   });
+// // });
+// (async () => {
+//   const data = await getHTML("testServiceID", "2023-01-01");
+//   console.log(`DATA IS THIS: ${data}`);
+// })();
